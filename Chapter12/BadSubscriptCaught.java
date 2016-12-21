21a4bfdc0f593a9042097758426ac7e52083959e
Fernando1Borges <centraloro@gmail.com>
2016-12-21 10:58:40 -0600

    Add bad subscript caught solution

---
+++ Chapter12/BadSubscriptCaught.java
@@ -0,0 +1,61 @@
+/*
+ * Created by Konrad Gnat
+ * Course Number: CIS263AA
+ * Section Number: 36454
+ * MEID: KON2155430
+ * On: December 21, 2016 
+ * Chapter: 12
+ * Page: 667
+ * Exercise: 1
+ * Title: Java Programming: Level II 
+ *
+ * Description:
+ * Write an application named BadSubscriptCaught in which you declare array of 10
+ * first names. Write a try block in which you prompt the user for an integer and
+ * display the name in the requested position. Create a catch block that catches
+ * the potential ArrayIndexOutOfBoundsException thrown when the user enters a
+ * number that is out of range. The catch block should also display an error
+ * message. Save the file as BadSubscriptCaught.java.
+ * 
+ * 
+ * 
+ */   
+import javax.swing.*;
+ 
+ 
+public class BadSubscriptCaught
+{
+    public static void main(String[] args) 
+    {
+        String[] tenFirstNames = {
+            "Tom", "Jackie", "Arnold", "Billy", "Ted", "Zach", "Joseph", "Mary", 
+            "Dennis", "Luke"
+        };
+        
+        try 
+        {
+         
+            String nameString = JOptionPane.showInputDialog(null, 
+                "Enter a number between 1 and 10 to display first name");
+            int nameInt = Integer.parseInt(nameString);
+            JOptionPane.showMessageDialog(null, tenFirstNames[nameInt - 1]);
+        
+        }
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(null, 
+                "Incorrect entry, you must enter a number between 1 and 10");
+        }
+        
+    }
+}
+
+
+
+
+
+ 
+
+
+
+
