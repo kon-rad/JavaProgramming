7c3e70ebef2fbf8bdac9245a6d55b1b108df12a6
Fernando1Borges <centraloro@gmail.com>
2017-01-05 19:44:37 -0600

    Add chapter 15 you do it solution

---
+++ Chapter15/JBorderLayout.java
@@ -0,0 +1,48 @@
+/*
+ * Created by Konrad Gnat
+ * Course Number: CIS263AA
+ * Section Number: 36454
+ * MEID: KON2155430
+ * On: January 05, 2017 
+ * Chapter: 15
+ * Page: 818
+ * Title: Java Programming: Level II 
+ *
+ * You Do It
+ * Using BorderLayout
+ * 
+ * 
+ * 
+ */   
+import javax.swing.*; 
+import java.awt.*; 
+ 
+public class JBorderLayout extends JFrame {  
+    // Data Fields
+    private JButton nb = new JButton("North");
+    private JButton sb = new JButton("South");
+    private JButton eb = new JButton("East");
+    private JButton wb = new JButton("West");
+    private JButton cb = new JButton("Center");
+    
+    public JBorderLayout() 
+    {
+        // Create JFrame
+        //super("...");
+        setLayout(new BorderLayout());
+        add(nb, BorderLayout.NORTH);
+        add(sb, BorderLayout.SOUTH);
+        add(eb, BorderLayout.EAST);
+        add(wb, BorderLayout.WEST);
+        add(cb, BorderLayout.CENTER);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);      
+    }
+    
+    public static void main(String[] args)
+    { 
+         // Run Application
+         JBorderLayout jbl = new JBorderLayout();
+         jbl.setSize(250, 250);
+         jbl.setVisible(true);
+    } 
+}
